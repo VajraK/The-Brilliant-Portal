2891953caf2f9e13be8bad4bc36eda59d8b769e6
VajraK <vajrakantor@gmail.com>
2024-10-09 09:25:38 +0200

    Adding Blog

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import 'leaflet/dist/leaflet.css';
-import './App.css';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Home from './pages/Home';
-import Blog from './pages/Blog';
+import Blog from './pages/blog/Blog';
 import Map from './pages/Map';
 import Animation from './pages/Animation';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ea213a',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>The Brilliant Portal</h1>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/map">Map</Link></li>
-            </ul>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/animation" element={<Animation />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              The Brilliant Portal
+            </Typography>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/blog">Blog</Button>
+            <Button color="inherit" component={Link} to="/map">Map</Button>
+          </Toolbar>
+        </AppBar>
+        <Container sx={{ mt: 3 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/animation" element={<Animation />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 }
 
